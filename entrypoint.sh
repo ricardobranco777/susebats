95cb8587382be36b9bee52807121fd91d8774b7a
Ricardo Branco <rbranco@suse.de>
2024-07-11 09:48:50 +0200

    Add susebats to run Docker image as script

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,9 @@ shift
 
 case "$command" in
 	bats_jobs|bats_list|bats_notok)
-		exec /"$command" "$@" ;;
+		exec "/$command" "$@" ;;
+	jobs|list|notok)
+		exec "/bats_$command" "$@" ;;
 	*)
 		echo >&2 "ERROR: Unknown command: $command"
 		echo >&2 "ERROR: Usage: bats_jobs|bats_list|bats_notok ..." ;;

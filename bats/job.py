738170fecb3b6e5bd6ca01c87a66bb1427f3dd47
Ricardo Branco <rbranco@suse.de>
2024-07-23 09:24:44 +0200

    Minor refactor

--- bats/job.py
+++ bats/job.py
@@ -5,7 +5,7 @@ Job module
 import os
 import sys
 from dataclasses import dataclass
-from urllib.parse import parse_qs, urlparse
+from urllib.parse import parse_qs, urljoin, urlparse
 
 import requests
 from requests.exceptions import RequestException
@@ -23,29 +23,27 @@ class Job:
     """
 
     name: str
-    logs: list[str] | None
+    logs: list[str]
     result: str
     results: list[dict] | None
     url: str
 
 
-def get_job_id(
-    url_string: str, params: dict[str, list[str]] | None = None
-) -> int | None:
+def get_job_id(url: str, params: dict[str, list[str]] | None = None) -> int | None:
     """
     Get job ID from URL with no job ID in URL
     """
-    url = urlparse(url_string)
-    if not url.query:
-        return int(os.path.basename(url.path).removeprefix("t"))
+    urlx = urlparse(url)
+    if not urlx.query:
+        return int(os.path.basename(urlx.path).removeprefix("t"))
 
-    api_url = f"{url.scheme}://{url.netloc}/api/v1/jobs/overview"
+    api_url = f"{urlx.scheme}://{urlx.netloc}/api/v1/jobs/overview"
     try:
         got = session.get(api_url, params=params, timeout=_TIMEOUT)
         got.raise_for_status()
         data = got.json()
     except RequestException as error:
-        print(f"ERROR: {url_string}: {error}", file=sys.stderr)
+        print(f"ERROR: {url}: {error}", file=sys.stderr)
         return None
     if len(data) != 1:
         return None
@@ -53,25 +51,23 @@ def get_job_id(
     return data[0]["id"]
 
 
-def get_job(
-    url_string: str, full: bool = False, build: str | None = None
-) -> Job | None:
+def get_job(url: str, full: bool = False, build: str | None = None) -> Job | None:
     """
     Get a job
     """
-    if not url_string.startswith(("http:", "https:")):
-        url_string = f"https://{url_string}"
-    url = urlparse(url_string)
+    if not url.startswith(("http:", "https:")):
+        url = f"https://{url}"
+    urlx = urlparse(url)
 
-    params: dict[str, list[str]] = parse_qs(url.query)
+    params: dict[str, list[str]] = parse_qs(urlx.query)
     if build:
         params["build"] = [build]
 
-    job_id = get_job_id(url_string, params=params)
+    job_id = get_job_id(url, params=params)
     if job_id is None:
         return None
 
-    api_url = f"{url.scheme}://{url.netloc}/api/v1/jobs/{job_id}"
+    api_url = f"{urlx.scheme}://{urlx.netloc}/api/v1/jobs/{job_id}"
     if full:
         api_url = f"{api_url}/details"
     try:
@@ -85,10 +81,13 @@ def get_job(
     if build and info["settings"]["BUILD"] != build:
         return None
 
+    url = f"{urlx.scheme}://{urlx.netloc}/tests/{job_id}"
+    logs = [urljoin(f"{url}/", f"file/{log}") for log in info.get("ulogs", [])]
+
     return Job(
-        logs=info.get("ulogs"),
+        logs=logs,
         name=info["name"],
         result=info["result"] if info["result"] != "none" else info["state"],
         results=info.get("testresults"),
-        url=f"{url.scheme}://{url.netloc}/tests/{job_id}",
+        url=url,
     )

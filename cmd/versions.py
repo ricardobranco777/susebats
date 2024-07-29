214a740893166dffe5ea286087b2ef69f324f931
Ricardo Branco <rbranco@suse.de>
2024-07-29 15:54:33 +0200

    versions: Print RPM version

--- cmd/versions.py
+++ cmd/versions.py
@@ -7,23 +7,41 @@ import argparse
 import re
 import sys
 from concurrent.futures import ThreadPoolExecutor
+from dataclasses import dataclass
 
 from bats.job import get_job, Job
-from bats.suse import fetch_version
+from bats.suse import fetch_version, RPMVersion
 
 
-VERSION = re.compile(
-    "(aardvark(?:-dns)?|buildah|netavark|podman|runc|skopeo) (?:info|version)"
-)
+PACKAGES = r"(aardvark-dns|buildah|netavark|podman|runc|skopeo)"
+GIT_VERSION = re.compile(rf"{PACKAGES} version$")
+RPM_VERSION = re.compile(rf"{PACKAGES} package version$")
 
 
-def get_version(title: str, info: str) -> tuple[str, str]:
+@dataclass
+class Version:
     """
-    Get version
+    Version class
+    """
+
+    git_version: str
+    rpm_version: str
+
+
+def get_rpm_version(info: str) -> tuple[str, str]:
+    """
+    Get RPM version from package name
+    """
+    assert info.endswith((".aarch64", ".noarch", ".ppc64le", ".s390x", ".x86_64"))
+    package, version, release = info.rsplit(".", 1)[0].rsplit("-", 2)
+    return package, f"{version}-{release}"
+
+
+def get_git_version(title: str, info: str) -> tuple[str, str]:
+    """
+    Get git version
     """
     package = title.split()[0]
-    if package == "aardvark":
-        package = "aardvark-dns"
     version = ""
     lines = info.splitlines()
     for line in lines:
@@ -36,6 +54,51 @@ def get_version(title: str, info: str) -> tuple[str, str]:
         else:
             version = lines[0].split()[-1]
     return package, version
+
+
+def get_versions(results: list[dict]) -> dict[str, Version]:
+    """
+    Get the git & RPM version for packages in openQA results
+    """
+
+    versions: dict[str, Version] = {}
+
+    # This needs to be done in openQA for this function to work, in this order:
+    #   record_info("podman version", script_output("podman version"));
+    #   record_info("podman package version", script_output("rpm -q podman"));
+    for result in results:
+        # Skip TAP parser output
+        if result["has_parser_text_result"]:
+            continue
+        package = git_version = rpm_version = ""
+        for detail in result["details"]:
+            if "title" not in detail:
+                continue
+            if GIT_VERSION.match(detail["title"]):
+                _, git_version = get_git_version(detail["title"], detail["text_data"])
+            elif RPM_VERSION.match(detail["title"]):
+                package, rpm_version = get_rpm_version(detail["text_data"])
+                break
+        if package:
+            versions[package] = Version(
+                git_version=git_version, rpm_version=rpm_version
+            )
+
+    return versions
+
+
+def get_published(product: str, packages: list[str]) -> dict[str, RPMVersion]:
+    """
+    Get published RPM versions for packages in product
+    """
+    published = {}
+    with ThreadPoolExecutor(max_workers=min(10, len(packages))) as executor:
+        for package, rpm_version in zip(
+            packages, executor.map(lambda p: fetch_version(product, p), packages)
+        ):
+            if rpm_version is not None:
+                published[package] = rpm_version
+    return published
 
 
 def main_versions(args: argparse.Namespace) -> None:
@@ -53,29 +116,28 @@ def print_versions(job: Job, verbose: bool = False) -> None:
     """
     Print job
     """
-    versions = {}
-    for result in job.results:
-        if not result["has_parser_text_result"]:
-            for detail in result["details"]:
-                if "title" not in detail:
-                    continue
-                if VERSION.match(detail["title"]):
-                    package, version = get_version(detail["title"], detail["text_data"])
-                    if package not in versions:
-                        versions[package] = version
+    versions = get_versions(job.results)
+    published = {}
 
-    rpm_versions = {}
+    fields = ["PACKAGE", "TAG", "TESTED"]
+    fmt = "{:<12}  {:<12}  {:<30}"
+    if verbose:
+        fmt += "  {}"
+        fields.append("PUBLISHED")
+
     if verbose:
         product = job.name.split("-Build")[0]
-        with ThreadPoolExecutor(max_workers=min(10, len(versions))) as executor:
-            for package, rpm_version in zip(
-                versions, executor.map(lambda p: fetch_version(product, p), versions)
-            ):
-                if rpm_version is not None:
-                    rpm_versions[package] = rpm_version
+        published = get_published(product, list(versions.keys()))
+    print(fmt.format(*fields))
 
     for package in sorted(versions):
-        if package in rpm_versions:
-            print(f"{package:<12}  {versions[package]:<12}  {rpm_versions[package]}")
-        else:
-            print(f"{package:<12}  {versions[package]:<12}")
+        print(
+            fmt.format(
+                *[
+                    package,
+                    versions[package].git_version,
+                    versions[package].rpm_version,
+                    published.get(package, ""),
+                ][: len(fields)]
+            )
+        )

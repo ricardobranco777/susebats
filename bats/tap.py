bcd29d1459b8037ec7e31961ff9fb51c7fae00dc
Ricardo Branco <rbranco@suse.de>
2025-04-27 12:29:05 +0200

    Extract test number from podman tests

--- bats/tap.py
+++ bats/tap.py
@@ -2,9 +2,48 @@
 tap module
 """
 
+import fnmatch
 import os
 import re
+import sys
 from collections import defaultdict
+from functools import cache
+
+from bats.requests import get_json
+from bats.versions import TEST_URL
+
+
+@cache
+def list_tests(package: str, version: str) -> list[str]:
+    """
+    List tests from upstream
+    """
+
+    repo_regex = r"^https://github.com/(.*)/blob/v{}/(.*)/{}\.bats$"
+    repo, test_dir = re.findall(repo_regex, TEST_URL[package])[0]
+
+    tag = version
+    if tag[0].isdigit() and not tag.startswith("v"):
+        tag = f"v{tag}"
+    elif tag == "latest":
+        api_url = f"https://api.github.com/repos/{repo}/tags"
+        data = get_json(api_url)
+        if data is None:
+            sys.exit(f"ERROR: {package} {tag}")
+        tag = data[0]["name"]
+
+    api_url = f"https://api.github.com/repos/{repo}/contents/{test_dir}"
+    params = {"ref": tag}
+    data = get_json(api_url, params=params)
+    if data is None:
+        sys.exit(f"ERROR: {package} {tag}")
+
+    items = []
+    for item in data:
+        if not item["name"].endswith(".bats"):
+            continue
+        items.append(item["name"].removesuffix(".bats"))
+    return items
 
 
 def grep_notok(file: str, alles: bool = True) -> dict[str, list[str]]:
@@ -18,11 +57,23 @@ def grep_notok(file: str, alles: bool = True) -> dict[str, list[str]]:
     buffer: list[str] = []
     tests = defaultdict(list)
 
+    package = ""
+
     for line in lines:
         if line.startswith(("not ok", "#not ok")):
+            # Sometimes, bats failures in podman don't show the "in test file" in the else below
+            # so extract "130" from "not ok 295 [130] podman kill - print IDs or raw input"
+            try:
+                number = re.findall(r"#?not ok \d+ \[(\d+)\] .*", line)[0]
+                package = "podman"
+                # FIX ME: We have to use real version
+                test = fnmatch.filter(list_tests(package, "latest"), f"{number}-*")[0]
+            except IndexError:
+                pass
             if test and buffer:
                 tests[test].append("\n".join(buffer) + "\n")
-            test = ""
+            if package != "podman":
+                test = ""
             buffer = [line]
         elif line.startswith("ok"):
             if test and buffer:

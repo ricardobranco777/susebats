775b566eba5aef40ce0cdaeaeb4c38073e4379f1
Ricardo Branco <rbranco@suse.de>
2026-04-11 22:16:32 +0200

    Ignore OOM since it is tested

--- susebats.py
+++ susebats.py
@@ -333,10 +333,8 @@ def get_traces(job: Job) -> list[str]:
         for detail in result["details"]
         if "title" in detail and detail["title"] == "TRACE"
     ]
-    package = job.settings.get("BATS_PACKAGE", "")
-    # Ignore OOM failures in runc since these are expected
-    if package == "runc":
-        traces = list(filter(lambda t: "mem_cgroup_out_of_memory" not in t, traces))
+    # Ignore OOM failures
+    traces = list(filter(lambda t: "mem_cgroup_out_of_memory" not in t, traces))
     return traces
 
 
